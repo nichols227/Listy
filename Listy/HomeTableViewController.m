5d129eb18e1db93ffeb87c60ce11ccf8faac47a3
nichols227 <ns@u.northwestern.edu>
2016-01-22 15:33:30 -0600

    Gradient added

--- Listy/HomeTableViewController.m
+++ Listy/HomeTableViewController.m
@@ -18,9 +18,29 @@
 }
 
 #define HEADER_HEIGHT 50
+#define FIRST_NAV_COLOR [UIColor colorWithRed:40/255.0 green:197/255.0 blue:231/255.0 alpha:.8f].CGColor
+#define LAST_NAV_COLOR [UIColor colorWithRed:0 green:153/255.0 blue:1 alpha:.8f].CGColor
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    CAGradientLayer* navGradient = [CAGradientLayer layer];
+    CGRect tempFrame = self.navigationController.navigationBar.bounds;
+    tempFrame.size.height += 20;
+    navGradient.frame = tempFrame;
+    navGradient.colors = @[ (id)FIRST_NAV_COLOR, (id)LAST_NAV_COLOR];
+    navGradient.startPoint = CGPointMake(1, .5);
+    navGradient.endPoint = CGPointMake(.5, .5);
+    UIGraphicsBeginImageContext(navGradient.bounds.size);
+    [navGradient renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *gradientImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    if (gradientImage != nil) {
+        [self.navigationController.navigationBar setBackgroundImage:gradientImage forBarMetrics:UIBarMetricsDefault];
+    } else {
+        NSLog(@"Failure");
+    }
+    
     
     headers = [[NSArray alloc] initWithObjects:@"Personal", @"Groups", @"Benjamins", nil];
     
---
+++ Listy/SplashPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+SplashPageViewController.h
+//  Listy
+//
+//  Created by Nicholas Irwin Scoliard on 1/22/16.
+//  Copyright © 2016 Nick Scoliard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashPageViewController : UIViewController
+
+@end
